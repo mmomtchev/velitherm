8d1bb9dfa75aa1a6726f64db9ee712cec8acb06e
Momtchil Momtchev <momtchil@momtchev.com>
2025-08-22 19:28:48 +0100

    run the C++ test and add a C test (#319)
    * run the C++ test and add a C test
    
    * try compiling as C99
    
    * link math?
    
    * update the README for C

--- src/gen_cpp.ts
+++ src/gen_cpp.ts
@@ -9,18 +9,23 @@ process.stdout.write(`
 
 #ifdef __cplusplus
 namespace velitherm {
+#define VELITHERM_DEFAULT(x) = x
+#else
+#define VELITHERM_DEFAULT(x)
 #endif
 
 `);
 
 process.stdin.on('data', (chunk) => {
   process.stdout.write(chunk.toString()
-    .replace(/export\s+function/g, 'inline double')
+    .replace(/([a-zA-Z0-9]+):\s+number\s+=\s+([A-Z0-9]+)/g,
+      'double $1 VELITHERM_DEFAULT($2)')
+    .replace(/export\s+function/g, 'static inline double')
     .replace(/([a-zA-Z0-9]+):\s+number/g, 'double $1')
     .replace(/\):\s+number/g, ')')
-    .replace(/export\s+const/g, 'double')
+    .replace(/export\s+const/g, 'const')
     .replace(/.+velitherm\s+=.+/g, '')
-    .replace(/const\s+([a-zA-Z0-9]+)/g, 'double $1')
+    .replace(/const\s+([a-zA-Z0-9]+)/g, 'const double $1')
     .replace(/Math\./g, ''));
 });
 
