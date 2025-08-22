4f148943d01ebe5b9b76450995787891f1a566d2
Momtchil Momtchev <momtchil@momtchev.com>
2025-08-22 13:09:04 +0100

    migrate to eslint9 (#313)

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import mocha from 'eslint-plugin-mocha';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default defineConfig([ globalIgnores([ 'dist/*', '**/*.js' ]), {
+  extends: compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ),
+
+  plugins: {
+    mocha,
+    '@typescript-eslint': typescriptEslint
+  },
+
+  languageOptions: {
+    globals: {
+      ...globals.mocha,
+      ...globals.node
+    },
+
+    parser: tsParser,
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  }
+} ]);
